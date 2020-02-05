2acc1efe751f7dbeb28c0aa7d59e0bd72228914b
ljh9527 <670684985@qq.com>
2020-02-05 16:24:24 +0800

    添加...

--- src/pages/detail/loadable.js
+++ src/pages/detail/loadable.js
@@ -4,7 +4,7 @@ import Loadable from 'react-loadable';
 const LoadableComponent = Loadable({
   loader: () => import('./'),
   loading() {
-    return <div>正在加载</div>
+    return <div>正在加载...</div>
   },
 });
 
